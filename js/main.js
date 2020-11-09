0c3fb1de9d44627f9d84618689cbedbc07d15ebf
77million <elena.kulakova2010@yandex.ru>
2020-11-09 18:07:19 +0300

    Create: mobile menu

--- js/main.js
+++ js/main.js
@@ -1,20 +1,28 @@
 $(document).ready(function () {
-  var modalButton = $("[data-toggle=modal]");
-  var CloseModalButton = $(".modal__close");
-  modalButton.on("click", openModal);
-  CloseModalButton.on("click", closeModal); 
+  // var modalButton = $("[data-toggle=modal]");
+  // var CloseModalButton = $(".modal__close");
+  // modalButton.on("click", openModal);
+  // CloseModalButton.on("click", closeModal); 
 
-  function openModal() {
-    var targetModal = $(this).attr("data-href");
-    $(targetModal).find(".modal__overlay").addClass("modal__overlay--visible");
-    $(targetModal).find(".modal__dialog").addClass("modal__dialog--visible");
-  }
+  // function openModal() {
+  //   var targetModal = $(this).attr("data-href");
+  //   $(targetModal).find(".modal__overlay").addClass("modal__overlay--visible");
+  //   $(targetModal).find(".modal__dialog").addClass("modal__dialog--visible");
+  // }
 
-  function closeModal(event) {
-    event.preventDefault();
-    var modalOverlay = $(".modal__overlay");
-    var modalDialog = $(".modal__dialog");
-    modalOverlay.removeClass("modal__overlay--visible");
-    modalDialog.removeClass("modal__dialog--visible");
-  }
+  // function closeModal(event) {
+  //   event.preventDefault();
+  //   var modalOverlay = $(".modal__overlay");
+  //   var modalDialog = $(".modal__dialog");
+  //   modalOverlay.removeClass("modal__overlay--visible");
+  //   modalDialog.removeClass("modal__dialog--visible");
+  // }
+
+  // mobile menu
+  var menuButton = document.querySelector(".menu-button");
+  menuButton.addEventListener('click', function () {
+    console.log('Клик по кнопке меню')
+    // $(".navbar-bottom").toggleClass('navbar-bottom_visible');
+  });
+
 });
