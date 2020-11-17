b6f9c230182ffa67f00c723dd3780a00215376db
77million <elena.kulakova2010@yandex.ru>
2020-11-17 22:27:22 +0300

    create: modal window finished

--- js/main.js
+++ js/main.js
@@ -1,22 +1,22 @@
 $(document).ready(function () {
-  // var modalButton = $("[data-toggle=modal]");
-  // var CloseModalButton = $(".modal__close");
-  // modalButton.on("click", openModal);
-  // CloseModalButton.on("click", closeModal); 
+  var modalButton = $("[data-toggle=modal]");
+  var CloseModalButton = $(".modal__close");
+  modalButton.on("click", openModal);
+  CloseModalButton.on("click", closeModal); 
 
-  // function openModal() {
-  //   var targetModal = $(this).attr("data-href");
-  //   $(targetModal).find(".modal__overlay").addClass("modal__overlay--visible");
-  //   $(targetModal).find(".modal__dialog").addClass("modal__dialog--visible");
-  // }
+  function openModal() {
+    var targetModal = $(this).attr("data-href");
+    $(targetModal).find(".modal__overlay").addClass("modal__overlay--visible");
+    $(targetModal).find(".modal__dialog").addClass("modal__dialog--visible");
+  }
 
-  // function closeModal(event) {
-  //   event.preventDefault();
-  //   var modalOverlay = $(".modal__overlay");
-  //   var modalDialog = $(".modal__dialog");
-  //   modalOverlay.removeClass("modal__overlay--visible");
-  //   modalDialog.removeClass("modal__dialog--visible");
-  // }
+  function closeModal(event) {
+    event.preventDefault();
+    var modalOverlay = $(".modal__overlay");
+    var modalDialog = $(".modal__dialog");
+    modalOverlay.removeClass("modal__overlay--visible");
+    modalDialog.removeClass("modal__dialog--visible");
+  }
 
   // mobile menu
   var menuButton = document.querySelector(".menu-button");
