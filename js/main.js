ff6103d3c0fbe1e580c35d3c93c30e99032ea7ad
77million <elena.kulakova2010@yandex.ru>
2020-12-23 16:43:39 +0300

    create: video-modal

--- js/main.js
+++ js/main.js
@@ -18,6 +18,26 @@ $(document).ready(function () {
     modalDialog.removeClass("modal__dialog--visible");
   }
 
+  // video-modal
+  var modalButton = $("[data-toggle=video-modal]");
+  var CloseModalButton = $(".video-modal__close");
+  modalButton.on("click", openModal);
+  CloseModalButton.on("click", closeModal); 
+
+  function openModal() {
+    var targetModal = $(this).attr("data-href");
+    $(targetModal).find(".video-modal__overlay").addClass("video-modal__overlay--visible");
+    $(targetModal).find(".video-modal__dialog").addClass("video-modal__dialog--visible");
+  }
+
+  function closeModal(event) {
+    event.preventDefault();
+    var modalOverlay = $(".video-modal__overlay");
+    var modalDialog = $(".video-modal__dialog");
+    modalOverlay.removeClass("video-modal__overlay--visible");
+    modalDialog.removeClass("video-modal__dialog--visible");
+  }
+
   // mobile menu
   var menuButton = document.querySelector(".menu-button");
   menuButton.addEventListener("click", function () {
